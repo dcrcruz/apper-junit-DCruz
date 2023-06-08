ebb6c065d297c185d70a795d5f150f4efbfe4364
derick <cruzderickr@gmail.com>
2023-06-08 11:41:34 +0800

    test commit

--- src/test/java/com/gcash/AccountRepositoryTest.java
+++ src/test/java/com/gcash/AccountRepositoryTest.java
@@ -1,2 +1,62 @@
-package com.gcash;public class AccountRepositoryTest {
+package com.gcash;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountRepositoryTest {
+    @Test
+    void successfulAccountCreation() {
+        //Setup
+        AccountRepository repository = new AccountRepository();
+
+        //Kick
+        String accountId = repository.createAccount("Derick", 89.9);
+
+        //Verify
+        Assertions.assertEquals(1, repository.getNumberOfAccounts());
+        Assertions.assertEquals("Derick", repository.getAccount(accountId).name());
+    }
+
+    @Test
+    void successfulGetAccount() {
+        AccountRepository repository = new AccountRepository();
+
+        String accountId = repository.createAccount("Derick",89.9);
+
+        Assertions.assertEquals("Derick", repository.getAccount(accountId).name());
+        Assertions.assertEquals(89.9, repository.getAccount(accountId).balance());
+        Assertions.assertEquals(null, repository.getAccount("randomid"));
+    }
+
+    @Test
+    void successfulDelete() {
+        //Setup
+        AccountRepository repository = new AccountRepository();
+        String id = repository.createAccount("Derick", 89.9);
+
+        //Kick
+        repository.deleteAccount(id);
+
+        //Verify
+        Assertions.assertEquals(0, repository.getNumberOfAccounts());
+    }
+
+    @Test
+    void successfulGetNumberofAccounts() {
+        //Verify and kick
+        AccountRepository repository = new AccountRepository();
+        String id0 = repository.createAccount("Derick", 89.9);
+        String id1 = repository.createAccount("Derick", 89.9);
+        String id2 = repository.createAccount("Derick", 89.9);
+        String id3 = repository.createAccount("Derick", 89.9);
+
+        //Verify
+        Assertions.assertEquals(4, repository.getNumberOfAccounts());
+
+        //Setup
+        repository.deleteAccount(id0);
+
+        //Verify
+        Assertions.assertEquals(3, repository.getNumberOfAccounts());
+    }
 }
