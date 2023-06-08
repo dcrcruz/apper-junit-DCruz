0b648a56e8616fb790641971dac1b2e7a0d5a193
derick <cruzderickr@gmail.com>
2023-06-08 12:49:05 +0800

    added pass/fail test to AccountRepository.java

--- src/main/java/com/gcash/AccountRepository.java
+++ src/main/java/com/gcash/AccountRepository.java
@@ -4,6 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+//failed test
+//public class AccountRepository {
+//    private final List<Account> accounts = new ArrayList<>();
+//
+//    public String createAccount(String name, Double initialBalance) {
+//        String id = UUID.randomUUID().toString();
+//        Account account = new Account(id, name, initialBalance);
+//
+//        accounts.add(account);
+//
+//        return id;
+//    }
+//
+//    public Account getAccount(String id) {
+//
+//        for (Account account : accounts) {
+//            if (account.id().equals(id)) {
+//                return account;
+//            }
+//        }
+//
+//        return null;
+//    }
+//
+//    public void deleteAccount(String id) {
+//        for (Account account : accounts) {
+//            if (account.id().equals(id)) {
+//                accounts.remove(account);
+//            }
+//        }
+//    }
+//
+//    public Integer getNumberOfAccounts() {
+//        return accounts.size();
+//    }
+//}
+
+//pass test
 public class AccountRepository {
     private final List<Account> accounts = new ArrayList<>();
 
@@ -17,22 +55,19 @@ public class AccountRepository {
     }
 
     public Account getAccount(String id) {
-
-        for (Account account : accounts) {
-            if (account.id().equals(id)) {
-                return account;
-            }
-        }
-
-        return null;
+        return accounts
+                .stream()
+                .filter(account -> id.equals(account.id()))
+                .findFirst()
+                .orElse(null);
     }
 
     public void deleteAccount(String id) {
-        for (Account account : accounts) {
-            if (account.id().equals(id)) {
-                accounts.remove(account);
-            }
-        }
+        accounts
+                .stream()
+                .filter(account -> id.equals(account.id()))
+                .findFirst()
+                .ifPresent(accounts::remove);
     }
 
     public Integer getNumberOfAccounts() {
